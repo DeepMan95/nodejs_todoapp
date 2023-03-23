0ab9ff5be0b8d236d6db2fddcb1feafc2d8a01ee
DeepMan95 <128705522+DeepMan95@users.noreply.github.com>
2023-03-23 18:33:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
+app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
-);
+}))
+
 
 // Using routes
 app.use("/api/v1/users", userRouter);
