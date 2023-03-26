b688794404e6f1dc6656fe131df4c8963c9bc5a1
DeepMan95 <128705522+DeepMan95@users.noreply.github.com>
2023-03-26 14:55:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://todo-app-thedpmane.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
